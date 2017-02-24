9ffbe5228a52aed2cb102dd984a24b83751f8152
Jay DiNitto <jay@jaydinitto.com>
2017-02-24 15:10:59 -0500

    updated js

--- js/tables_jquery.js
+++ js/tables_jquery.js
@@ -1,52 +1,41 @@
-// Code goes here
-//find the widest cell in each column in a series of 2 tables or more
-//for perfectly aligned/non-jaggy cells
-//good for tables that may have different lengths of data
-
-//must pass in the id of direct parent element of table array
-//must also have <th> cells in first row
-//like any good table has
-
-//ex: setWidestCell('thisIsTheParent')
-//or: setWidestCell('#thisIsTheParent');
-
-function setWidestCelljquery(el) {
-
+var setWidestCell = (function (el) {
+  'use strict'
+  
+  //add hashtag to id if none is passed
   var addHash = function() {
-      if (el.indexOf('#') === -1) { 
-        el = '#' + el
-      }
+      if (el.indexOf('#') === -1) {el = '#' + el}
       return el;
     },
     
+    //get the widest
     arrayMax = function(array) {
       return Math.max.apply(Math, array);
+    },
+
+    //iterate through all the tables
+    loopThroughTables = function(getWidest){
+      $(tablesToFix).each(function() {
+        if (getWidest) {
+          return widthArr.push($(this).find(cell).width());
+        }
+        else {
+          return $(this).find(cell).width(arrayMax(widthArr));
+        }
+      });
     },
     
     parentElId = addHash(),
     tablesToFix = parentElId + ' table',
     allCols = $(tablesToFix + ' thead tr:first').find('th'),
-
-    getWidest = function() {
-
+    
+    getAndSetWidest = function() {
       for (i = 0; i < allCols.length; i++) {
         var cell = 'thead tr th:eq(' + i + ')',
-          widthArr = [];
-
-        $(tablesToFix).each(function() {
-          widthArr.push($(this).find(cell).width());
-        });
- 
-        $(tablesToFix).each(function() {
-          $(this).find(cell).width(arrayMax(widthArr));
-        });
-
+        widthArr = [];
+        loopThroughTables(true); //get widest
+        loopThroughTables(false); //set 'em all
       }
     };
-
-  return getWidest();
-}
-
-$(document).ready( function() {
-  setWidestCell('thisIsTheParent');
+    
+    return;
 });
