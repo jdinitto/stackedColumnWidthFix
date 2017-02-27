f2c5ffcf08ed9d6830bb07032c08b39d91c7c5ee
Jay DiNitto <jay@jaydinitto.com>
2017-02-27 15:39:54 -0500

    updated finalish js

--- js/tables_vanilla.js
+++ js/tables_vanilla.js
@@ -1,41 +1,29 @@
 function setWidestCelljs(el) {
 
-  var widthArr = [],
+    var allTables = document.getElementById(el).getElementsByTagName('table'),
+        allCols = allTables[0].getElementsByTagName('tbody')[0].getElementsByTagName('tr')[0].getElementsByTagName('td'),
 
-    allTables = document.getElementById(el)
-    .getElementsByTagName('table'),
+        //get the largest width out of the width array
+        arrayMax = function(array) {
+            return Math.max.apply(Math, array);
+        },
 
-    allCols = allTables[0].getElementsByTagName('thead')[0]
-    .getElementsByTagName('tr')[0]
-    .getElementsByTagName('th'),
+        //iterate through all the tables
+        loopThroughTables = function(getWidest, arr, i) {
+            for (x = 0; x < allTables.length; x++) {
+                var cell = allTables[x].getElementsByTagName('tbody')[0].getElementsByTagName('tr')[0].getElementsByTagName('td')[i];
+                //return the widest cell width, or set to that widest cell
+                getWidest ? arr.push(cell.offsetWidth) : cell.style.width = arrayMax(arr) + 'px';
+            }
+        },
 
-    arrayMax = function(array) {
-      return Math.max.apply(Math, array);
-    },
+        getAndSetWidest = function() {
+            for (i = 0; i < allCols.length; i++) {
+                var widthArr = [];
+                loopThroughTables(true, widthArr, i); //get widest
+                loopThroughTables(false, widthArr, i); //set 'em all
+            }
+        };
 
-    getWidest = function() {
-
-      for (i = 0; i < allCols.length; i++) {
-        widthArr = [];
-
-        for (x = 0; x < allTables.length; x++) {
-          var cell = allTables[x].getElementsByTagName('thead')[0]
-            .getElementsByTagName('tr')[0]
-            .getElementsByTagName('th')[i];
-
-          widthArr.push(cell.offsetWidth);
-        }
-
-        for (x = 0; x < allTables.length; x++) {
-          var cell = allTables[x].getElementsByTagName('thead')[0]
-            .getElementsByTagName('tr')[0]
-            .getElementsByTagName('th')[i];
-
-          cell.style.width = arrayMax(widthArr) + 'px';
-        }
-
-      }
-    };
-
-  return getWidest();
+    return getAndSetWidest();
 }
