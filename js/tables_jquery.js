61ae1354a89a8aad7afb9c883d1f6e9cb5535eff
Jay DiNitto <jay@jaydinitto.com>
2017-02-27 15:39:18 -0500

    updated finalish js

--- js/tables_jquery.js
+++ js/tables_jquery.js
@@ -1,41 +1,39 @@
-var setWidestCell = (function (el) {
-  'use strict'
-  
-  //add hashtag to id if none is passed
-  var addHash = function() {
-      if (el.indexOf('#') === -1) {el = '#' + el}
-      return el;
-    },
-    
-    //get the widest
-    arrayMax = function(array) {
-      return Math.max.apply(Math, array);
-    },
+var setWidestCelljquery = function(el) {
+    'use strict';
 
-    //iterate through all the tables
-    loopThroughTables = function(getWidest){
-      $(tablesToFix).each(function() {
-        if (getWidest) {
-          return widthArr.push($(this).find(cell).width());
-        }
-        else {
-          return $(this).find(cell).width(arrayMax(widthArr));
-        }
-      });
-    },
-    
-    parentElId = addHash(),
-    tablesToFix = parentElId + ' table',
-    allCols = $(tablesToFix + ' thead tr:first').find('th'),
-    
-    getAndSetWidest = function() {
-      for (i = 0; i < allCols.length; i++) {
-        var cell = 'thead tr th:eq(' + i + ')',
-        widthArr = [];
-        loopThroughTables(true); //get widest
-        loopThroughTables(false); //set 'em all
-      }
-    };
-    
-    return;
-});
+    //add hashtag to id if none is passed
+    var addHash = function() {
+            if (el.indexOf('#') === -1) {
+                el = '#' + el
+            }
+            return el;
+        },
+
+        //get the widest
+        arrayMax = function(array) {
+            return Math.max.apply(Math, array);
+        },
+
+        //iterate through all the tables
+        loopThroughTables = function(getWidest, cell, arr) {
+            $(tablesToFix).each(function() {
+                //return the widest cell width, or set to that widest cell
+                getWidest ? arr.push($(this).find(cell).width()) : $(this).find(cell).width(arrayMax(arr));
+            });
+        },
+
+        parentElId = addHash(),
+        tablesToFix = parentElId + ' table',
+        allCols = $(tablesToFix + ' tbody tr:first').find('td'),
+
+        getAndSetWidest = function() {
+            $(allCols).each(function(i) {
+                var cell = 'tbody tr td:eq(' + i + ')',
+                    widthArr = [];
+                loopThroughTables(true, cell, widthArr); //get widest
+                loopThroughTables(false, cell, widthArr); //set 'em all
+            });
+        };
+
+    return getAndSetWidest();
+};
